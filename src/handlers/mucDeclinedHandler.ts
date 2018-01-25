bd2d9dc760c0c80af3e632a4e9d74aa5b549d0d9
Robert Crossland <robertcrossland@gmail.com>
2018-01-25 14:12:12 -0600

    added the mucDeclinedHandler and tests and fixed some cosmetic things in other files

--- src/handlers/mucDeclinedHandler.ts
+++ src/handlers/mucDeclinedHandler.ts
@@ -2,15 +2,16 @@ import Handler from './handler';
 import { Subject } from 'rxjs/Subject';
 import * as colors from 'colors';
 
+
 export default class MucAvailableHandler extends Handler<string> {
     constructor() {
         super();
 
-        this.name = 'muc:available';
+        this.name = 'muc:declined';
     }
-
+ 
     handler = (data: any): void => {
-        console.info(colors.green('#'), colors.green(`-- ${this.name} --`));
+        console.info(colors.red('!'), colors.red(`-- ${this.name} --`));
         this.subject.next(this.name);
     };
 }
---
+++ test/handlers/mucDeclinedHandler.test.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import * as proxyquire from 'proxyquire';
+import Handler from '../../src/handlers/handler';
+
+describe('The MucDeclinedHandler class', () => {
+    let handler: any;
+
+    beforeEach(() => {
+        const MucDeclinedHandler  = proxyquire('../../src/handlers/mucDeclinedHandler', {}).default;
+
+        handler = new MucDeclinedHandler ();
+    });
+
+    it('should exist', () => {
+        // arrange
+        // act
+        // assert
+        expect(handler).to.exist;
+    });
+
+    it('should be a Handler type', () => {
+        // arrange
+        // act
+        // assert
+        expect(handler instanceof Handler).to.be.true;
+    });
+
+    describe('The name member', () => {
+        it('should be set to the string \'muc:declined\'', () => {
+            // arrange
+            // act
+            // assert
+            expect(handler.name).to.equal('muc:declined');
+        });
+    });
+
+    describe('The handler method', () => {
+        it('should exist', () => {
+            // arrange
+            // act
+            // assert
+            expect(handler.handler).to.exist;
+        });
+
+        it('should call next on the subject when hit', () => {
+            // arrange
+            sinon.spy(handler.subject, 'next');
+
+            // act
+            handler.handler({});
+
+            // assert
+            expect(handler.subject.next.calledWithExactly(handler.name)).to.be.true;
+
+            handler.subject.next.restore();
+        });
+    });
+});
